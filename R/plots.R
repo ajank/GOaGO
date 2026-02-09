6a1b2a19a71a0459fc5eceaf5d54cdd1ef14e5d2
Aleksander Jankowski <aleksander.jankowski@uw.edu.pl>
2026-02-09 15:36:03 +0100

    Revised `RidgePlot`

--- R/plots.R
+++ R/plots.R
@@ -1,10 +1,45 @@
-# sort by enrichment ratio, and impose the same ordering on the label levels
-
-sortedResult <- function(goago) {
+# Internal function to select only the terms that are associated to at least
+# `minTermPairs` gene pairs, and convert the terms (both `ID` and `Description`
+# columns) to factors ordered by the column `orderBy`. Finally, select only the
+# number of terms (or the terms themselves) specified by `showCategory`.
+.sortedResult <- function(
+    object,
+    minTermPairs = 5,
+    showCategory = 10,
+    orderBy = "FoldEnrichment",
+    decreasing = TRUE
+) {
     dt <- as.data.table(goago)
-    dt[, label := Description]
-    dt <- dt[order(Ratio / BgRatio), ]
-    dt[, label := factor(label, unique(label))]
+    dt <- dt[Count >= minTermPairs, ]
+
+    if (!orderBy %in% colnames(dt)) {
+        message("wrong orderBy parameter: ", orderBy)
+    }
+
+    # note the reverse order, so that the terms show up as expected
+    # (top-to-bottom, reverse to the scale direction) in the final plot
+    ind <- order(dt[[orderBy]], decreasing = !decreasing)
+    dt[, ID := factor(ID, ID[ind])]
+    dt[, Description := factor(Description, Description[ind])]
+
+    if (is.numeric(showCategory)) {
+        dt <- head(dt, showCategory)
+    } else {
+        dt <- dt[ID %in% showCategory, ]
+    }
+
+    return(dt)
+}
+
+# Internal function to wrap labels at a given wrap length, or use a labeller
+# function provided.
+.label_format <- function(label_format = 50) {
+    if (is.function(label_format)) {
+        label_func <- label_format
+    } else {
+        label_func <- label_wrap_gen(label_format)
+    }
+    return(label_func)
 }
 
 
@@ -13,10 +48,10 @@ sortedResult <- function(goago) {
 ##' The x-axis shows the log~2~ fold change of the fraction of gene pairs
 ##' sharing the given terms. Color indicates the adjusted p-value.
 ##'
-##' @param goago GO-a-GO results of class \code{GOaGO-result}
-##' @param minTermPairs take only the GO terms that are associated to at least
+##' @param object GO-a-GO results of class \code{GOaGO-result}
+##' @param minTermPairs plot only the GO terms that are associated to at least
 ##'   the given number of gene pairs
-##' @returns A ggplot object that can be further customized using the
+##' @returns A \code{ggplot} object that can be further customized using the
 ##'   \code{ggplot2} package.
 ##' @export
 ##' @examples
@@ -29,8 +64,8 @@ sortedResult <- function(goago) {
 ##' )
 ##'
 ##' DotPlot(goago)
-DotPlot <- function(goago, minTermPairs = 5) {
-    dt <- sortedResult(goago)
+DotPlot <- function(object, minTermPairs = 5) {
+    dt <- .sortedResult(goago)
     dt <- dt[Count >= minTermPairs, ]
 
     p <- ggplot(dt, aes(
@@ -44,15 +79,24 @@ DotPlot <- function(goago, minTermPairs = 5) {
 }
 
 
-##' Ridgeplot of the sampling distributions for the randomized gene pairs
+##' Ridgeplot of the sampling distributions for the randomized gene pairs.
 ##'
 ##' Ridgeplot of the sampling distributions of numbers of gene pairs sharing
 ##' each Gene Ontology term, obtained for the randomized gene pairs.
 ##'
-##' @param goago GO-a-GO results of class \code{GOaGO-result}
-##' @param minTermPairs take only the GO terms that are associated to at least
+##' @param object GO-a-GO results of class \code{GOaGO-result}
+##' @param minTermPairs plot only the GO terms that are associated to at least
 ##'   the given number of gene pairs
-##' @returns A ggplot object that can be further customized using the
+##' @param showCategory number of terms to display or a vector of terms
+##' @param orderBy The order of the Y-axis, one of \code{"FoldEnrichment"},
+##'   \code{"Ratio"} and \code{"Count"}.
+##' @param decreasing logical. Should the \code{orderBy} order be increasing or
+##'   decreasing?
+##' @param font.size font size
+##' @param label_format a numeric value sets wrap length, alternatively a custom
+##'   function to format axis labels. By default wraps names longer than 50
+##'   characters.
+##' @returns A \code{ggplot} object that can be further customized using the
 ##'   \code{ggplot2} package.
 ##' @export
 ##' @examples
@@ -65,27 +109,44 @@ DotPlot <- function(goago, minTermPairs = 5) {
 ##' )
 ##'
 ##' RidgePlot(goago)
-RidgePlot <- function(goago, minTermPairs = 5) {
-    dt <- sortedResult(goago)
-    dt <- dt[Count >= minTermPairs, ]
-
-    dt2 <- merge(dt[, c("ID", "label")], goago@permutedResult, by = "ID")
-
-    lab <- c(
-        `TRUE` = "Number of actual gene pairs\nsharing a Gene Ontology term",
-        `FALSE` = "not significant"
+RidgePlot <- function(
+    object,
+    minTermPairs = 5,
+    showCategory = 10,
+    orderBy = "FoldEnrichment",
+    decreasing = TRUE,
+    font.size = 12,
+    label_format = 50
+) {
+    dt <- .sortedResult(object,
+        minTermPairs = minTermPairs,
+        showCategory = showCategory, orderBy = orderBy, decreasing = decreasing
     )
-    p <- ggplot(dt2, aes(x = Count, y = label)) +
+
+    dt_permuted <- merge(dt[, c("ID", "Description")],
+        object@permutedResult,
+        by = "ID"
+    )
+
+    label_func <- .label_format(label_format)
+
+    col_color <- c(`TRUE` = "#e41a1c", `FALSE` = "black")
+    col_fill <- c(`TRUE` = NA, `FALSE` = "gray70")
+    lab <- c(`TRUE` = "actual gene pairs", `FALSE` = "randomized gene pairs")
+
+    p <- ggplot(
+        dt_permuted,
+        aes(x = Count, y = Description, color = FALSE, fill = FALSE)
+    ) +
         geom_density_ridges() +
-        geom_point(data = dt, aes(color = p.adjust < 0.05)) +
-        scale_color_manual(NULL,
-            values = c(`TRUE` = "#e41a1c", `FALSE` = "#666666"),
-            labels = lab
-        ) +
-        xlab("Number of randomized gene pairs\nsharing a Gene Ontology term") +
-        ylab(NULL) +
-        guides(color = guide_legend(ncol = 1)) +
-        theme(legend.position = "bottom") +
-        NULL
+        geom_point(data = dt, aes(x = Count, color = TRUE, fill = TRUE)) +
+        scale_color_manual(NULL, values = col_color, labels = lab) +
+        scale_fill_manual(NULL, values = col_fill, labels = lab) +
+        labs(x = "Number of gene pairs sharing a term", y = NULL) +
+        scale_y_discrete(labels = label_func) +
+        theme_dose(font.size) +
+        theme(axis.text.y = element_text(lineheight = 0.9)) +
+        theme(legend.position = "bottom")
+
     return(p)
 }
