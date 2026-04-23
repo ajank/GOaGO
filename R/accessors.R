0b707ee5e8369c859a9c1ed06c94231c707068a6
Aleksander Jankowski <aleksander.jankowski@uw.edu.pl>
2026-04-23 21:36:36 +0200

    Use all gene pairs for examples

--- R/accessors.R
+++ R/accessors.R
@@ -30,8 +30,7 @@ setAs("GOaGO-result", "data.frame", function(from) {
 ##' library(org.Hs.eg.db)
 ##' data("genePairsGM12878")
 ##'
-##' genePairsSubset <- subset(genePairsGM12878, chrom1 == "chr11")
-##' goago <- GOaGO(genePairsSubset, keyType = "ENTREZID", OrgDb = org.Hs.eg.db)
+##' goago <- GOaGO(genePairsGM12878, keyType = "ENTREZID", OrgDb = org.Hs.eg.db)
 ##' as.data.frame(goago)
 setMethod(
     "as.data.frame", signature(x = "GOaGO-result"),
@@ -60,8 +59,7 @@ setMethod(
 ##' library(org.Hs.eg.db)
 ##' data("genePairsGM12878")
 ##'
-##' genePairsSubset <- subset(genePairsGM12878, chrom1 == "chr11")
-##' goago <- GOaGO(genePairsSubset, keyType = "ENTREZID", OrgDb = org.Hs.eg.db)
+##' goago <- GOaGO(genePairsGM12878, keyType = "ENTREZID", OrgDb = org.Hs.eg.db)
 ##' show(goago)
 ##'
 ##' genePairs(goago)
